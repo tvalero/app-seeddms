da74abaec67a1d179c02ddbb397cb36c278b04fe
Thierry VALERO (IRD)- MPLCLTMP0191 <thierry.valero@ird.fr>
2019-08-07 18:05:44 +0200

    End of day

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cp images/seeddms/configs/settings.xml  conf/settings.xml
 mkdir -p data/var/lib/mysql
 mkdir -p data/var/www/seeddms51x/data/lucene
 mkdir -p data/var/www/seeddms51x/data/staging
