02def26a0c4b19a16c95f4ec29a2517fe9205494
Thierry VALERO (IRD/MIVEGEC) <thierry.valero@ird.fr>
2019-08-08 10:57:41 +0200

    Fin de la journrnée

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 mkdir -p conf
+mkdir -p data/seeddms
+mkdir -p data/mysql
+
 cp images/seeddms/configs/settings.xml  conf/settings.xml
+
 mkdir -p data/var/lib/mysql
-mkdir -p data/var/www/seeddms51x/data/lucene
-mkdir -p data/var/www/seeddms51x/data/staging
+mkdir -p data/lucene
+mkdir -p data/staging
+
 chmod -R 777 data
 docker-compose up -d
