d78f7a61c33c4cdc23469f0dbee6cd24e2bd61d9
Thierry Valero (Vectobol) <thierry.valero@ird.fr>
2019-08-21 08:43:39 -0400

    as is.

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 mkdir -p conf
+
 mkdir -p data/seeddms
 mkdir -p data/seeddms/lucene
 mkdir -p data/seeddms/staging
-
 mkdir -p data/mysql
 
-cp images/seeddms/configs/settings.xml  conf/settings.xml
-
-mkdir -p data/mysql
-
+cp images/seeddms/configs/settings.xml   conf/settings.xml
+cp images/seeddms/configs/stopwords.txt  conf/stopwords.txt
+touch									 conf/ENABLE_INSTALL_TOOL
 chmod -R 777 data
 #docker-compose up -d
